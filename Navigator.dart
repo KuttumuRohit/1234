159b2cfb9d22090939f7cb0d42fabfaec082592f
Hitman_17 <116746857+KuttumuRohit@users.noreply.github.com>
2024-12-28 08:09:57 +0530

    Create Navigator.dart

---
+++ Navigator.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Row widget",
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => secondscreen()),
+            );
+          },
+          child: const Text("Go to Second Page"),
+        ),
+      ),
+    );
+  }
+}
+
+class secondscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Go to Home Page"),
+        ),
+      ),
+    );
+  }
+}
